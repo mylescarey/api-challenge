28efecf1f5a0a7311900ab136ade543888c06172
Myles Carey <myles.carey@Nology.local>
2019-05-08 10:29:19 +0100

    Two hard coded books displaying on the website

--- src/components/book/book.tsx
+++ src/components/book/book.tsx
@@ -9,22 +9,36 @@ class Book extends React.Component<IProps, IState> {
   // state = { :  }
   public render() {
     return (
-      <div className={styles.scene}>
-        <span className={styles.card}>
-          <header>Still Me</header>
-          <p>Author: Jojo Moyes</p>
-          <p>
-            THE NO. 1 SUNDAY TIMES BESTSELLER & SHORTLISTED FOR A NATIONAL BOOK
-            AWARD 'A triumph' Heat __________ Lou Clark knows too many things .
-            . . She knows how many miles lie between her new home in New York
-            and her new boyfriend Sam in London. She knows her employer is a
-            good man and she knows his wife is keeping a secret from him. What
-            Lou doesn't know is she's about to meet someone who's going to turn
-            her whole life upside down. Because Josh will remind her so much of
-            a man she used to know that it'll hurt.
-          </p>
-        </span>
-      </div>
+      <React.Fragment>
+        <div className={styles.scene}>
+          <span className={styles.card}>
+            <div className={styles.header}>
+              <h1>Still Me</h1>
+            </div>
+            <h2>Author: Jojo Moyes</h2>
+            <p>
+              THE NO. 1 SUNDAY TIMES BESTSELLER & SHORTLISTED FOR A NATIONAL
+              BOOK AWARD 'A triumph' Heat __________ Lou Clark knows too many
+              things . . . She knows how many miles lie between her new home in
+              New York and her new boyfriend Sam in London.
+            </p>
+          </span>
+        </div>{" "}
+        <div className={styles.scene}>
+          <span className={styles.card}>
+            <div className={styles.header}>
+              <h1>Still Me</h1>
+            </div>
+            <h2>Author: Jojo Moyes</h2>
+            <p>
+              THE NO. 1 SUNDAY TIMES BESTSELLER & SHORTLISTED FOR A NATIONAL
+              BOOK AWARD 'A triumph' Heat __________ Lou Clark knows too many
+              things . . . She knows how many miles lie between her new home in
+              New York and her new boyfriend Sam in London.
+            </p>
+          </span>
+        </div>
+      </React.Fragment>
     );
   }
 }
